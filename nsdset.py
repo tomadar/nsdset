b056f5644c8616c908bdb8746f1b45de5e9c6a84
tobi_martins <62524527+tomadar@users.noreply.github.com>
2025-08-25 16:30:09 +0100

    Create nsdset.py

---
+++ nsdset.py
@@ -0,0 +1,31 @@
+import os
+Import time
+
+def set():
+	try:
+		os.system('clear')
+		print('''
+		Number Seperation and Differences , classical division differences table
+''')
+		n = int(input("Number: "))
+		d = int(input("Difference: "))
+		s = int(input("Seperation: "))
+		try:
+			f = n/s - ((d/s)+((d*s)/2)-d)
+			print(f)
+			for i in range(s-1):
+				f += d
+				print(f)
+		except ZeroDivisionError:
+			print("Sorry, Zero is Indenominable!")
+			time.sleep(2)
+			os.system('clear')
+			set()
+
+	except ValueError:
+		print("Only number supported!")
+		time.sleep(2)
+		os.system('clear')
+		set()
+
+set()
