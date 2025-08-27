4f6c2c473da3c6532209c490357217a23a5ac5f4
tobi_martins <62524527+tomadar@users.noreply.github.com>
2025-08-27 13:29:08 +0100

    Update nsd.py

--- nsd.py
+++ nsd.py
@@ -1,3 +1,3 @@
 import os
-os.system("chmod +x */nsdset/nsdset.py")
-os.system("move */nsdset/nsdset.py /usr/local/bin/nsdset")
+os.system("chmod +x nsdset/nsdset.py")
+os.system("move nsdset/nsdset.py /usr/local/bin/nsdset")
